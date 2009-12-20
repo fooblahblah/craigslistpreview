7528be1996964da355f036ba87cdc21a3483208f
Jeff Simpson <jsimpson@crapple.local>
2009-12-19 21:31:44 -0700

    Changed fetch strategy to be incremental

--- src/cl.js
+++ src/cl.js
@@ -1,5 +1,7 @@
 $(function() 
 {
+    var hrefs = [];
+
     $('p span.p').each(
 	function() 
 	{
@@ -12,21 +14,35 @@ $(function()
 	    var entryDiv =$(this).parent().wrap('<div class="entryDiv"></div>').
 		parent().get(0);
 	    var clPrevImg = $(entryDiv).append('<div class="clPrevImg"></div>').find(".clPrevImg");
-	    
-	    getAdPage($(a).attr("href"),
-		      function(data)
-		      {
-			  $(data).find('img:lt(4)').appendTo($(clPrevImg));
-  			  $(entryDiv).find('.clPrevImg > img').each(
-			      function()
-			      {
-				  $(this).wrap('<a href="' + $(a).attr('href') + '"></a>');
-			      }
-			  );
-		      });
+	    hrefs.push({ href: $(a).attr("href"), div: $(clPrevImg) });
 	}
     );   
+    
+    getImages(hrefs);
 });
+
+getImages = function(hrefs) 
+{
+    var obj = hrefs.shift();
+
+    if(!obj) 
+    {
+	return;
+    }
+
+    getAdPage(obj.href,
+	      function(data)
+	      {
+		  $(data).find('img:lt(4)').appendTo(obj.div).each(
+		      function()
+		      {
+			  $(this).wrap('<a href="' + obj.href + '"></a>');
+		      }
+		  );
+		  
+		  $.doTimeout(50, function() { getImages(hrefs); });
+	      });
+}
 
 getAdPage = function(href, callback)
 {
